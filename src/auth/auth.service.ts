419c65d94c6c70b5ceaedd82fa2fa4f906a981b4
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-06-25 14:01:27 +0530

    Added basic create , fetch, fetch all methods in service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/schema/user.schema';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(@InjectModel('user') private readonly userModel: Model<UserDocument>) { };
+
+    /**
+     * creates a new user
+     */
+    async createUser(user: User): Promise<User> {
+        const newUser = new this.userModel(user);
+        return newUser.save();
+    }
+
+    /**
+     * Fetches a single user and logs in.
+     */
+    async fetchUser(userReq: User): Promise<User> {
+        let result;
+        const email = userReq.email;
+        try {
+            const user = await this.userModel.findOne({ email });
+            if (user) {
+                result = user;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        return result;
+    }
+
+    /**
+     * Fetches all users
+     */
+    async fetchAllUsers(): Promise<void> {
+        let result;
+        try {
+            const userList = await this.userModel.find({});
+            if (userList) {
+                result = userList;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        return result;
+    }
+}
