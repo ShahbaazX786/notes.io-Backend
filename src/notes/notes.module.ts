db3df75ac5608646690c41d96169420232b7b803
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-06-26 13:53:41 +0530

    Updated note module with mongoosemodel via schema

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NotesSchema } from 'src/schema/note.schema';
 import { NotesController } from './notes.controller';
 import { NotesService } from './notes.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Note', schema: NotesSchema }])],
   controllers: [NotesController],
   providers: [NotesService]
 })
-export class NotesModule {}
+export class NotesModule { }
