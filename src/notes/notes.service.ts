1fcc9a7f26e65a7f7ac85fdb51b3de9983338f55
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-06-26 14:09:34 +0530

    Added createnote service method.

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { NoteResponseDto } from 'src/dto/noteResponseDto';
+import { Note } from 'src/schema/note.schema';
 
 @Injectable()
-export class NotesService {}
+export class NotesService {
+    constructor(
+        @InjectModel(Note.name)
+        private noteModel: mongoose.Model<Note>) { };
+
+    async createNote(note): Promise<any> {
+        let response: NoteResponseDto;
+        try {
+            const res = await this.noteModel.create(note);
+            response = {
+                success: true,
+                message: 'Note created successfully',
+                data: res,
+            }
+        } catch (error) {
+            console.log(error);
+            response = {
+                success: false,
+                message: `Error Creating Notes: ${error.message}`,
+            }
+        }
+        return response;
+    }
+}
