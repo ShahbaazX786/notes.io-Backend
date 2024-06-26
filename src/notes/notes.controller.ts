9a6b998b662672c7fa54f25ccdedbf11b08fb146
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-06-26 14:09:22 +0530

    Added create Note method

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,8 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { NotesService } from './notes.service';
+import { Note } from 'src/schema/note.schema';
 
 @Controller('notes')
 export class NotesController {
     constructor(private readonly notesService: NotesService) { }
 
+    @Post()
+    async addNote(@Body() note: Note) {
+        const result = await this.notesService.createNote(note);
+        return result;
+    }
 }
