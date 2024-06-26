f47cc6245ef50382b133687d67ba763254005389
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-06-26 13:43:39 +0530

    Added utilities module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UtilitiesModule } from './utilities/utilities.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     MongooseModule.forRoot(process.env.DB_URI),
     AuthModule,
+    UtilitiesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
