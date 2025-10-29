df0fdb3e76020418411fa00459de199bcd7d4e8d
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-29 08:43:18 +0530

    added global pipes for user req validation.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const validationConfig = {
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  };
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe(validationConfig));
+
+  await app.listen(3200).then(() => console.log('Server running on port 3200'));
 }
 bootstrap();
