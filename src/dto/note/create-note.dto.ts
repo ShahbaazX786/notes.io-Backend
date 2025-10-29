6e65d9b9c120b7ebe5ddf25868f4ff88f9e7ca11
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-29 09:58:05 +0530

    Added a create note dto.

---
+++ src/dto/note/create-note.dto.ts
@@ -0,0 +1,29 @@
+import {
+  ArrayMaxSize,
+  ArrayUnique,
+  IsArray,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateNoteDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 30)
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(500)
+  description?: string;
+
+  @IsOptional()
+  @IsArray()
+  @ArrayMaxSize(5)
+  @ArrayUnique()
+  @IsString({ each: true })
+  tags?: string[];
+}
