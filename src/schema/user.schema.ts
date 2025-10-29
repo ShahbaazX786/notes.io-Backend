0983f1d10a69ef50b3941702c48d65be90250132
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-29 13:39:57 +0530

    Added bcrypt + passport for jwt.

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,15 +1,15 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema({ timestamps: true })
 export class User {
-    @Prop()
-    username: string;
+  @Prop()
+  username: string;
 
-    @Prop()
-    email: string;
+  @Prop()
+  email: string;
 
-    @Prop()
-    password: string;
+  @Prop()
+  password: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
