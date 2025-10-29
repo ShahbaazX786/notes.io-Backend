29f8ef04ccd0dbf1229140ad105cc73374f55ce1
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-10-29 13:25:58 +0530

    minor bug fix, it is string array not just string.

--- src/schema/note.schema.ts
+++ src/schema/note.schema.ts
@@ -1,15 +1,15 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema({ timestamps: true })
 export class Note {
-    @Prop()
-    title: string;
+  @Prop()
+  title: string;
 
-    @Prop()
-    description: string;
+  @Prop()
+  description: string;
 
-    @Prop()
-    tags: string;
+  @Prop()
+  tags: string[];
 }
 
 export const NotesSchema = SchemaFactory.createForClass(Note);
