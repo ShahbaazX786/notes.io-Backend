a8946becb7bfd328b373ba3f1346310403b6de41
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-05 12:15:32 +0530

    initialized jwt module.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/schema/user.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'villian',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
-  exports: [],
+  exports: [AuthService],
 })
 export class AuthModule {}
