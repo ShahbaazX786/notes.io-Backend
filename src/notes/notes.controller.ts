73ddd4e0e9b1fb791b1c4935e61d6ce3ee05df97
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-05 13:30:44 +0530

    Added route guards on all note endpoints.

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -7,45 +7,53 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { Query as ExpresQuery } from 'express-serve-static-core';
-import { NotesService } from './notes.service';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { CreateNoteDto } from 'src/dto/note/create-note.dto';
 import { UpdateNoteDto } from 'src/dto/note/update-note.dto';
+import { NotesService } from './notes.service';
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async addNote(@Body() note: CreateNoteDto) {
     const result = await this.notesService.createNote(note);
     return result;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('all')
   async getAllNotes(@Query() query: ExpresQuery) {
     const result = await this.notesService.findAll(query);
     return result;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async FetchNoteById(@Param('id') id: string) {
     const result = await this.notesService.getNote(id);
     return result;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   async updateNoteById(@Param('id') id: string, @Body() note: UpdateNoteDto) {
     const result = await this.notesService.updateNote(id, note);
     return result;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async DeleteNoteById(@Param('id') id: string) {
     const result = await this.notesService.deleteNote(id);
     return result;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete()
   async DeleteAllNotes() {
     const result = await this.notesService.deleteAllNotes();
