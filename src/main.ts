3441bf74d5b4bf2421c74f97dcf82804e01b1bbc
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-07 07:38:13 +0530

    enabled cors for local development.

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,14 @@ async function bootstrap() {
   };
   const app = await NestFactory.create(AppModule);
 
+  // remove during prod.
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true, // allows cookies if you use them later
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalPipes(new ValidationPipe(validationConfig));
 
   await app.listen(3200).then(() => console.log('Server running on port 3200'));
